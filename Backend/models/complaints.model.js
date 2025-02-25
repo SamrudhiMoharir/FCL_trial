d41769769e13c927a73a2c84bbc652a38d2be7f3
shivajirp <shivajirp123@gmail.com>
2025-02-25 16:54:29 +0530

    add car no

--- Backend/models/complaints.model.js
+++ Backend/models/complaints.model.js
@@ -1,5 +1,6 @@
 // models/Complaint.js
 import mongoose from 'mongoose';
+import { Counter } from './counter.model.js';
 
 const complaintSchema = new mongoose.Schema({
   companyCode: {
@@ -41,7 +42,30 @@ const complaintSchema = new mongoose.Schema({
     required: true,
     enum: ["pending", "in progress", "resolved"],
     default: "pending"
-  }
+  },
+  CARNo: { 
+    type: String, 
+    unique: true 
+  } 
 }, { timestamps: true });
+
+
+complaintSchema.pre("save", async function (next) {
+  if (!this.CARNo) {
+    const now = new Date();
+    const month = String(now.getMonth() + 1).padStart(2, "0");
+    const year = now.getFullYear();
+
+    const counter = await Counter.findOneAndUpdate(
+      { name: "CARNo" },
+      { $inc: { seq: 1 } },
+      { new: true, upsert: true }
+    );
+
+    const sequenceNumber = String(counter.seq).padStart(3, "0");
+    this.CARNo = `FCL/${month}/${year}/${sequenceNumber}`;
+  }
+  next();
+});
 
 export const Complaint = mongoose.model("Complaint", complaintSchema);
